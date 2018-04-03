74995cbdc2e7105ea71f6182c159d2d5b150c895
Bastian Winkler <bwinkler@sapient.com>
2018-04-03 16:51:12 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>This is a test</h1>
+    <div>{{ output }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      output: 0
+    };
+  },
+  mounted() {
+    console.log(this.test());
+    this.output = this.test();
+  },
+  methods: {
+
+    /** Some test async function
+     * @event something
+     * @type {Event}
+     */
+    test: async function() {
+      await this.$nextTick();
+      this.output = 1;
+    }
+  }
+};
+</script>
+<style>
+
+</style>
---
+++ styleguide.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          exclude: /node_modules/,
+          loader: "vue-loader"
+        },
+        // For js or css files:
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          loader: "style-loader!css-loader"
+        }
+      ]
+    }
+  }
+};
